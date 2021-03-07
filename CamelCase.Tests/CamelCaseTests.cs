0023744d04c92754135a309b98450a64c9ca1632
pBarsky <patrykbaranski12@gmail.com>
2021-03-07 17:39:53 +0100

    Add project files.

---
+++ CamelCase.Tests/CamelCaseTests.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace CamelCase.Tests
+{
+    public class CamelCaseTests
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
---
+++ CamelCase/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CamelCase
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Run all tests to check your solution.");
+            Console.WriteLine("(default Ctrl+R A)");
+        }
+    }
+}
